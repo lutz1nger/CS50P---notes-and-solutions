9cfc874737241bdeac729afacca12424c166420b
lutz1nger <christian.lutz91@gmx.de>
2026-01-19 17:36:59 +0100

    Update notes_1.py

--- Lecture 1 - Conditionals/notes_1.py
+++ Lecture 1 - Conditionals/notes_1.py
@@ -1,1 +1,21 @@
+#CS50P - Introduction to Programming with Python
 
+#notes: Lecture 1 - Conditionals
+
+#SYNTAX CONDITIONALS:
+#> 
+#>=
+#<
+#<=
+#==
+#!=
+
+#if every condition is asked
+
+#boolean expresion -> True(=1) and False(=0)
+
+#elif -> else if -> improvement
+
+#else
+
+#or
